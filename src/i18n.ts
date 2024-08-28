20a752aeaaf0a9ddec107333ade0b706467faed4
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2024-08-28 13:20:33 +0200

    i18n en

--- src/i18n.ts
+++ src/i18n.ts
@@ -1477,9 +1477,9 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    fallbackLng: "pl",
+    fallbackLng: "en",
     // debug: true,
-    lng: "pl",
+    lng: "en",
     //supportedLngs: ["pl", "en"],
     // fallbackLng: "pl",
     // lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
